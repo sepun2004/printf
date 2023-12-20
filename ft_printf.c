65e024528f071e0b933999e67d2b6ec0f63e4640
Sebastian Pun <sepun@c3r4s3.42madrid.com>
2023-12-20 15:24:00 +0100

    20 de Diciembre

--- ft_printf.c
+++ ft_printf.c
@@ -15,6 +15,16 @@
 #include <string.h>
 //#include "ft_printf.h"
 
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
 int ft_putchar(char argument, int count)
 {
     write(1, &argument, 1);
@@ -24,7 +34,7 @@ char ft_putstr(char *argument, int count)
 {
     
     int i = 0;
-    int len = strlen(argument);
+    int len = ft_strlen(argument);
     while (i < len)
     {
         ft_putchar(argument[i], count);
@@ -33,23 +43,29 @@ char ft_putstr(char *argument, int count)
     return(count);
 }
 
-void	ft_putnbr(int n)
+int	ft_putnbr(int n, int count)
 {
 	if (n == -2147483648)
-		ft_putstr("-2147483648");
-	else if (n < 0)
+    {
+		write (1, "-2147483648", 11);
+        return(count = 11);
+    }
+	if (n < 0)
 	{
-		ft_putchar('-');
-		n = n * (-1);
+		ft_putchar('-', count);
+		n = -n;
 	}
 	if (n > 9)
 	{
-		ft_putnbr(n / 10);
-		ft_putnbr(n % 10);
+		ft_putnbr(n / 10, count);
+		ft_putnbr(n % 10, count);
 	}
-	if (n <= 9 && n != -2147483648)
-		ft_putchar(n + '0');
+    else
+    {
+        ft_putchar(n + '0', count);
+    }
 }
+/*
 void	ft_puthexa(void *n)
 {
 	//if (n == -2147483648)
@@ -66,17 +82,15 @@ void	ft_puthexa(void *n)
 	//if (n <= 9 && n != -2147483648)
 	//	ft_putchar(n + '0');
 }
-//
+//*/
 int ft_select_type(va_list argument, char c, size_t count)
 {
     if (c == 'c')
-        count = ft_putchar(va_arg(argument, char), count);
+        count = ft_putchar(va_arg(argument, int), count);
     if (c == 's')
         count = ft_putstr(va_arg(argument, char *), count);
     if (c == 'i')
         count = ft_putnbr(va_arg(argument, int), count);
-    if (c == 'p')
-        count = ft_putpointer(va_arg(argument, void *), count);
     return(count);
 }
 //ft_print_all();
@@ -88,12 +102,12 @@ int ft_printf(char const *str, ...)
     va_list argument;
 
     va_start(argument, str);
-    while (str != '\0')
+    while (*str != '\0')
     {
-        if (str == '%')
+        if (*str == '%')
         {
-            i = ft_select_type(argument, str[i +1], i);
             i++;
+            i = ft_select_type(argument, str[i +1], i);
         }
         else
             i = ft_putchar(str[i], i);
@@ -105,13 +119,8 @@ int ft_printf(char const *str, ...)
 
 int main()
 {
-    char *s = NULL;
-    unsigned long i = (unsigned long)s;
-    
-    printf("%p\n", (void *)s);
-    printf("%lx", i);
-    
-    //0x559ca042b004
-    //55906db6d004
+    int x = 2;
+
+    ft_printf("%i", x);
     return 0;
 }
