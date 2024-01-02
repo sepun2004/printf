f867dfcb20fef05ccac5fe8198f6a523c3f589de
sepun2004 <145572771+sepun2004@users.noreply.github.com>
2024-01-02 14:05:39 +0100

    zzz

--- ft_printf.c
+++ ft_printf.c
@@ -13,17 +13,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-//#include "ft_printf.h"
 
-size_t	ft_strlen(const char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
 
 int ft_putchar(char argument, int count)
 {
@@ -34,7 +24,7 @@ char ft_putstr(char *argument, int count)
 {
     
     int i = 0;
-    int len = ft_strlen(argument);
+    int len = strlen(argument);
     while (i < len)
     {
         ft_putchar(argument[i], count);
@@ -45,26 +35,22 @@ char ft_putstr(char *argument, int count)
 
 int	ft_putnbr(int n, int count)
 {
-	if (n == -2147483648)
-    {
-		write (1, "-2147483648", 11);
-        return(count = 11);
-    }
 	if (n < 0)
 	{
-		ft_putchar('-', count);
-		n = -n;
+	    write(1, "-", 1);
+		n *= -1;
+		count++;
 	}
 	if (n > 9)
 	{
-		ft_putnbr(n / 10, count);
-		ft_putnbr(n % 10, count);
+		count = ft_putnbr(n / 10, count);
+		count = ft_putchar(n % 10 + '0', count);
 	}
-    else
-    {
-        ft_putchar(n + '0', count);
-    }
+	else
+		count = ft_putchar(n + '0', count);
+	return (count);
 }
+
 /*
 void	ft_puthexa(void *n)
 {
@@ -82,45 +68,51 @@ void	ft_puthexa(void *n)
 	//if (n <= 9 && n != -2147483648)
 	//	ft_putchar(n + '0');
 }
-//*/
+//
+*/
 int ft_select_type(va_list argument, char c, size_t count)
 {
     if (c == 'c')
         count = ft_putchar(va_arg(argument, int), count);
     if (c == 's')
         count = ft_putstr(va_arg(argument, char *), count);
-    if (c == 'i')
+    if (c == 'i'  || c == 'd')
         count = ft_putnbr(va_arg(argument, int), count);
+    /*
+    if (c == 'p')
+        count = ft_putpointer(va_arg(argument, void *), count);
+    */
     return(count);
 }
 //ft_print_all();
 
 
-int ft_printf(char const *str, ...)
+int	ft_printf(char const *str, ...)
 {
-    int i = 0;
-    va_list argument;
+	int		bytes;
+	va_list	args;
 
-    va_start(argument, str);
-    while (*str != '\0')
-    {
-        if (*str == '%')
-        {
-            i++;
-            i = ft_select_type(argument, str[i +1], i);
-        }
-        else
-            i = ft_putchar(str[i], i);
-        i++;
-    }
-    return (va_end(argument), i);
+	bytes = 0;
+	va_start(args, str);
+	while (*str != '\0')
+	{
+	    if (*str == '%')
+		{
+			str++;
+			bytes = ft_select_type(args, *str, bytes);
+		}
+		else
+			bytes = ft_putchar(*str, bytes);
+		str++;
+	}
+	return (va_end(args), bytes);
 }
 
 
 int main()
 {
-    int x = 2;
+    int c = ----123;
 
-    ft_printf("%i", x);
+    ft_printf("%i", c);
     return 0;
 }
